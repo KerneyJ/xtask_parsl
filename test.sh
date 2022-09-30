809d72f5bbfc657b5668ca0842a98f212eeb69a2
kerneyj <gamerguy9801@gmail.com>
2022-09-30 15:22:44 -0500

    increased fib for test

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 
 source ../forkenv/bin/activate && pip freeze | xargs pip uninstall -y
 ../forkenv/bin/python3 -m pip install ../parsl/.
-python3 func.py htex_lq fib 5 -d benchmarks/parsl_test
+python3 func.py htex_lq fib 15 -d benchmarks/parsl_test
