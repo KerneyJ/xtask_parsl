9bc8ec5e01a430040ff502eb7a58f9dd99eebf8a
kerneyj <gamerguy9801@gmail.com>
2022-12-12 18:24:19 +0000

    loop interchange in xqueue.sh

--- xqueue.sh
+++ xqueue.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 source ../xq/bin/activate
 
-for w in 1 2 4 8 16 32 64 128
+for n in $(seq -f "%02g" 5 5 30)
 do
-    for n in $(seq -f "%02g" 5 5 30)
-    do
-	    for _ in {1..10}
-	    do
-		    python3 func.py xq 1 $w fib $n -d benchmarks/xqueue >> xqfib${n}-$w.txt
-	    done
-    done
+	for w in 1 2 4 8 16 32 64 128
+	do
+		for _ in {1..10}
+		do
+			python3 func.py xq 1 $w fib $n -d benchmarks/xqueue >> xqfib${n}-$w.txt
+		done
+	done
 done
