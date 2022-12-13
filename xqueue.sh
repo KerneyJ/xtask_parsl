6928b62a3fa32c533b1b0794a6412e8e4cc360dd
kerneyj <gamerguy9801@gmail.com>
2022-12-13 20:33:15 +0000

    formatted files

--- xqueue.sh
+++ xqueue.sh
@@ -3,11 +3,11 @@ source ../xq/bin/activate
 
 for n in $(seq -f "%02g" 5 5 30)
 do
-	for w in 1 2 4 8 16 32 64 128
+	for w in 001 002 004 008 016 032 064 128
 	do
 		for _ in {1..10}
 		do
-			python3 func.py xq 1 $w fib $n -d benchmarks/xqueue >> xqfib${n}-$w.txt
+			python3 func.py xq 1 $w fib $n -d benchmarks/xqueue >> xqfib$n-$w.txt
 		done
 	done
 done
