33a8f2d7ab1c090e6f0b65b445504b49390d0bd5
kerneyj <gamerguy9801@gmail.com>
2023-03-02 00:07:16 -0600

    added test for dependencies in test_cdfk.py

--- test_cdfk.py
+++ test_cdfk.py
@@ -25,22 +25,38 @@ def add_args(a, b):
     return a + b
 
 @python_app
-def add_defargs(a=2, b=2):
+def add_kwargs(a=2, b=2):
     return a + b
 
+def print_fut_list(futs, args):
+    out = [f.result() for f in futs]
+    for index in range(len(out)):
+        if isinstance(out[index], RemoteExceptionWrapper):
+            try:
+                out[index].reraise()
+            except TypeError as e:
+                print(args[index], str(e))
+        else:
+            assert out[index] == 4
+            print(args[index], "passed")
+
+# Test passing arguments
+print("Testing argument passing")
+print("an => add no arguments\n" \
+        "aa => add arguments\n" \
+        "ak => add keyword arguments")
 nums = [add_args(2,2),
         add_args(2,2), add_args(2,b=2), add_args(a=2,b=2),
-        add_defargs(), add_defargs(2,2), add_defargs(a=2), add_defargs(b=2), add_defargs(2, b=2)]
+        add_kwargs(), add_kwargs(2,2), add_kwargs(a=2), add_kwargs(b=2), add_kwargs(2, b=2)]
 
-args = ["an()", "aa(2,2)", "aa(2,b=2)", "aa(a=2,b=2)", "ad()", "ad(2,2)", "ad(a=2)", "ad(b=2)", "ad(2, b=2)"]
+args = ["an()", "aa(2,2)", "aa(2,b=2)", "aa(a=2,b=2)", "ak()", "ak(2,2)", "ak(a=2)", "ak(b=2)", "ak(2, b=2)"]
+print_fut_list(nums, args)
 
-out = [n.result() for n in nums]
-for index in range(len(out)):
-    if isinstance(out[index], RemoteExceptionWrapper):
-        try:
-            out[index].reraise()
-        except TypeError as e:
-            print(args[index], str(e))
-    else:
-        assert out[index] == 4
-        print(args[index], " passed")
+print("\n\n",end='')
+# Test dependencies
+print("Testing dependencies")
+a = add()
+b = add_args(a, 3)
+c = add_args(a, 4)
+d = add_args(b, c)
+print_fut_list([a,b,c,d], ["a", "b", "c", "d"])
